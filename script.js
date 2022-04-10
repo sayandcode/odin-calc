57ba0382e5afd1a47976ec504c0b27fccf8280bc
sayandcode <sayandcode@gmail.com>
2022-04-10 13:54:32 +0530

    Add working buttons

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-const screen = document.querySelector('.screen');
-screen.addEventListener('keydown',checkValidInput);
-
+const calcScreen = document.querySelector('.screen');
+calcScreen.addEventListener('keydown',checkValidInput);
 
 function checkValidInput(e){
-	if(e.ctrlKey||e.altKey||typeof e.key!=='string'||e.key.length!==1)
+	if(e.ctrlKey||e.altKey||typeof e.key!=='string'||e.key.length!==1) //if special buttons, let it pass
         return;
 
-    if(!(/[\d\+\u00D7\u00F7\u2212]/.test(e.key))){
-		console.log('invalid');
-        e.preventDefault();
-    }
+    if(!(/[\d\+\u00D7\u00F7\u2212]/.test(e.key)))
+        e.preventDefault(); //filter out invalid input
 }
 
+const buttons=document.querySelector('.buttons');
+buttons.addEventListener('click',buttonPressed);
+
+function buttonPressed(e){
+    console.log(e.target.value);
+    calcScreen.value+=e.target.value;
+}
 // function add(a,b) {
 // 	return a+b;
 // }
