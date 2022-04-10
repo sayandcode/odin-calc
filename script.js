45e6001a9c2acf9ac6762545cf0294f00ccd872c
sayandcode <sayandcode@gmail.com>
2022-04-10 10:21:24 +0530

    Add basic structure of ui

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(a,b) {
+	return a+b;
+}
+
+function subtract(a,b) {
+	return a-b;
+}
+
+function multiply(array) {
+  return array.reduce((tally,curr)=>tally*curr);
+}
+
+function divide(a,b) {
+	return a/b;
+}
